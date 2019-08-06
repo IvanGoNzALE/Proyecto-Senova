96f8ba7b786db060732e3495389d68fdd3fb957e
IvanGoNzALE <50214115+IvanGoNzALE@users.noreply.github.com>
2019-08-06 15:22:43 -0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Proyecto_Senova
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int Edad;
+            int puntaje = 0;
+            Proyecto miproyecto = new Proyecto();
+            Proyecto miproyecto2 = new Proyecto();
+            Proyecto miproyecto3 = new Proyecto();
+            Proyecto miproyecto4 = new Proyecto("realidad aumentada",1549,"sistemas",12);
+            Proyecto miproyecto5 = new Proyecto("Talento humano",65);
+            //miproyecto3.IngresaDatos();
+            //miproyecto3.imprimir();
+            //miproyecto4.imprimir();
+            miproyecto5.imprimir();
+
+
+            //miproyecto.Area = "Sistemas";
+            //miproyecto.Codigo = 1804643;
+            //miproyecto.Duracion = 13;
+            //miproyecto.Nombre = "Desarrollo de internet de las cosas IOT";
+            //Console.WriteLine($"El area se llama:{miproyecto.Area}");
+            //Console.WriteLine($"El codigo es: {miproyecto.Codigo}");
+            //Console.WriteLine($"la duracion es de: {miproyecto.Duracion}horas");
+            //Console.WriteLine($"Su nombre es:{miproyecto.Nombre}");
+
+
+            Console.ReadLine();
+            Console.ReadKey();
+
+
+
+
+
+
+        }
+    }
+}
---
+++ Proyecto.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Proyecto_Senova
+{
+    class Proyecto
+    {
+        #region Atributos
+
+        #endregion
+
+
+        #region Propiedades
+        public string Nombre { get; set; }
+        public double Codigo { get; set; }
+        public string Area { get; set; }
+        public int Duracion { get; set; }
+        #endregion
+
+        #region Constructores
+        public Proyecto (string nombre, double codigo, string area, int duracion  )
+        {
+            Nombre = nombre;
+            Codigo = codigo;
+            Area = area;
+            Duracion = duracion;
+        }
+        public Proyecto()
+        {
+
+
+        }
+        public Proyecto (string area,int duracion)
+        {
+
+            Area = area;
+            Duracion = duracion;
+        }
+
+        #endregion
+
+        #region Metodos
+
+        public void IngresaDatos()
+        {
+            Console.WriteLine("ingrese el nombre del proyecto");
+            Nombre = Console.ReadLine();
+            Console.WriteLine("ingrese el codigo");
+            Codigo = double.Parse(Console.ReadLine());
+            Console.WriteLine("ingerse el area");
+            Area = Console.ReadLine();
+            Console.WriteLine("ingrese la duracion");
+            Duracion = int.Parse(Console.ReadLine());
+        }
+        public void imprimir()
+        {
+            Console.WriteLine($"El nombre es:{Nombre}");
+            Console.WriteLine($"El codigo es:{Codigo}");
+            Console.WriteLine($"El area es:{Area}");
+            Console.WriteLine($"La duracion es de:{Duracion} horas");
+        }
+        #endregion
+    }
+}
